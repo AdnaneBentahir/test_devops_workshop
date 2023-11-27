57dc2804c54027a2007873d2f8ad7da5cf8b360b
CHAMPALAUNE Maël <mael.champalaune@groupe-creative.fr>
2023-11-27 15:22:55 +0100

    fix

--- docker-images.sh
+++ docker-images.sh
@@ -2,6 +2,8 @@
 
 docker pull maven:3.8.5-openjdk-17
 docker pull openjdk:17.0.2-slim
+docker pull mariadb:10.6.13
+docker pull adminer:4.8.1
 docker pull sonarsource/sonar-scanner-cli:4.8.1
 docker pull owasp/zap2docker-stable:2.14.0
 #docker pull sonarqube:10.3.0-community
