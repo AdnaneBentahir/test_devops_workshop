2a418c278cc769419dc07222d8c5ffa5bcf04528
CHAMPALAUNE Maël <mael.champalaune@groupe-creative.fr>
2023-11-27 16:15:55 +0100

    add tools

---
+++ .platforms/tools/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ./.platforms/tools/run.sh
+# source .platforms/bootstrap.sh
+
+# Création des répertoires
+sudo mkdir -p /opt/workshop/jenkins/
+sudo mkdir -p /opt/workshop/nexus/nexus-data
+sudo mkdir -p /opt/workshop/sonarqube/conf
+sudo mkdir -p /opt/workshop/sonarqube/data
+sudo mkdir -p /opt/workshop/sonarqube/extensions
+sudo mkdir -p /opt/workshop/sonarqube/logs
+sudo mkdir -p /opt/workshop/sonarqube/temp
+sudo mkdir -p /opt/workshop/sonarqube/postgresql/data
+sudo chmod 777 -R /opt/workshop/
+
+# Lancement du projet
+docker-compose -p devops-tools -f .platforms/tools/docker-compose.yml up
